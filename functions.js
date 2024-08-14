44a3bbb23532b6b9ebdd96d0488d39d8c48b1877
FranSfw <theweatherreport33@gmail.com>
2024-08-14 13:40:24 -0700

    Actualizacion en el Hero

--- functions.js
+++ functions.js
@@ -1,4 +1,14 @@
 const contactButton = document.getElementById('contactButton');
+const heightDivImages = document.getElementById('divBussinesses').offsetHeight;
+const divImages = document.getElementById('divImages');
+
+window.addEventListener('load', function () {
+    divImages.style.height = `${heightDivImages}px`;
+});
+
+window.addEventListener('resize', function () {
+    divImages.style.height = `${heightDivImages}px`;
+});
 
 contactButton.addEventListener('click', function () {
     window.scrollTo({
