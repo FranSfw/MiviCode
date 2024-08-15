3015046880c6aedca3798dd501ab09a5e6eb632a
Maiky-ahoyo <arroyomiguel590@gmail.com>
2024-08-14 22:18:33 -0700

    Update index.html and contact.html

--- functions.js
+++ functions.js
@@ -1,14 +1,4 @@
 const contactButton = document.getElementById('contactButton');
-const heightDivImages = document.getElementById('divBussinesses').offsetHeight;
-const divImages = document.getElementById('divImages');
-
-window.addEventListener('load', function () {
-    divImages.style.height = `${heightDivImages}px`;
-});
-
-window.addEventListener('resize', function () {
-    divImages.style.height = `${heightDivImages}px`;
-});
 
 contactButton.addEventListener('click', function () {
     window.scrollTo({
